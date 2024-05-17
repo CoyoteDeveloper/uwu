a3ac991bf95533bb3aa1d3d487536d20b3ed3835
CoyoteDeveloper <74499310+CoyoteDeveloper@users.noreply.github.com>
2024-05-17 21:50:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res, next) => {
         })
     }
     );
-    res.sendFile(path.join(__dirname, 'Strona gwna.html'));
+    res.redirect('https://sites.google.com/view/friskportfolio');
     next();
 });
 
