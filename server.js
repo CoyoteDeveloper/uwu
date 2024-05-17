13d9cbc094322706cd6135dfaee8299a100573ed
CoyoteDeveloper <74499310+CoyoteDeveloper@users.noreply.github.com>
2024-05-17 19:21:01 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route for GET requests
+app.get('/', (req, res) => {
+    const ip = req.ip || req.connection.remoteAddress;
+    res.send(`Request IP: ${ip}`);
+});
+
+// Define another route for GET requests with a parameter
+app.get('/user/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`User ID: ${userId}`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
