2eae0be51f0c88df90ea6178d59c2b301745cc16
CoyoteDeveloper <74499310+CoyoteDeveloper@users.noreply.github.com>
2024-06-07 16:21:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,20 +6,21 @@ const port = 3000;
 app.set('trust proxy', true);
 // Define a route for GET requests
 app.get('/', (req, res, next) => {
-    const ip = req.header('x-forwarded-for') ||
-						req.socket.remoteAddress;
-    fetch(
-    "https://discord.com/api/webhooks/1241084793802850426/e-hMfQ1lVanHlHgavBGJESfwhshIxO0G9TfrZNoeI0GiuIjedUov7NvjHp00wDP2ySlQ", 
-    {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            content: ip,
-        })
-    }
-    );
+   const ip = req.header('x-forwarded-for') ||
+//						req.socket.remoteAddress;
+//    fetch(
+//    "https://discord.com/api/webhooks/1241084793802850426/e-hMfQ1lVanHlHgavBGJESfwhshIxO0G9TfrZNoeI0GiuIjedUov7NvjHp00wDP2ySlQ", 
+//    {
+//        method: "POST",
+//        headers: {
+//            'Content-Type': 'application/json'
+//        },
+//        body: JSON.stringify({
+//            content: ip,
+//        })
+//    }
+//    );
+    console.log(ip);
     res.redirect('https://sites.google.com/view/friskportfolio');
     next();
 });
